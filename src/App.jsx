04b27966c6482efece92dda32f12def0617be2fa
Dipanshu <dipanshuyadav9385@gmail.com>
2023-12-04 10:19:27 +0530

    password generate functionlity completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useCallback, useEffect, useState } from "react"
 
 
 function App() {
-  
+  const[length,setLength]=useState(4)
+  const[password,setPassword]=useState() 
+ const[numAllowed,setNumAllowed]=useState(false)
+ const[charAllowed,setCharAllowed]=useState(false)
+
+ const PasswordGenerator=useCallback(()=>{
+  let pass="";
+  let str="ABCDEFGHIJKLMNOPQURSTUVWXYZabcdefghijklmnopqurstuvwxyz"
+  if (numAllowed) str += "1234567890"
+  if (charAllowed) str += "!@#$%&*?+=/-_"
+  for (let i = 0; i <length; i++) {
+   let char=Math.floor(Math.random() * str.length +1)
+   pass += str.charAt(char)
+   
+  }
+  setPassword(pass)
+},[length,numAllowed,charAllowed,setPassword])
+
+useEffect(()=>{PasswordGenerator()},[PasswordGenerator,numAllowed,length,charAllowed])
 
   return (
    <div>
